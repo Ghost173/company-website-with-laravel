1b1a2e47b7b884a0879cd1acf197c61d7ffadf97
Ghost173 <jpirakas007@gmail.com>
2020-11-19 23:46:37 +0530

    logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('admin.index');
 })->name('dashboard');
+
+Route::get('user/logout', [AdminController::class, 'logout'])->name('user.logout');
